0aca91361a999ba95699cdf1f057781a257aa8c8
Kanika <kanuaggarwal2006@gmail.com>
2025-03-01 12:46:50 +0530

    adding tranversewithrecursion

--- linkedlist/SinglyLinkedList.cpp
+++ linkedlist/SinglyLinkedList.cpp
@@ -1,49 +1,89 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// // A linked list node
+// class Node {
+// public:
+//     int data;
+//     Node* next;
+
+//     // Constructor to initialize a new node with data
+//     Node(int new_data) {
+//         this->data = new_data;
+//         this->next = nullptr;
+//     }
+// };
+
+// // Function to traverse and print the singly linked list
+// void traverseList(Node* head) {
+
+//     // A loop that runs till head is nullptr
+//     while (head != nullptr) {
+
+//         // Printing data of current node
+//         cout << head->data << " ";
+
+//         // Moving to the next node
+//         head = head->next;
+//     }
+//     cout << endl;
+// }
+
+// // Driver Code
+// int main() {
+  
+//     // Create a hard-coded linked list:
+//     // 10 -> 20 -> 30 -> 40
+//     Node* head = new Node(10);
+//     head->next = new Node(20);
+//     head->next->next = new Node(30);
+//     head->next->next->next = new Node(40);
+//     head->next->next->next->next = new Node(50);
+//     head->next->next->next->next->next = new Node(60);
+
+//     // Example of traversing the node and printing
+//     traverseList(head);
+
+//     return 0;
+// }
+
+
+
+
+
+// done the program by himself
+#include<iostream>
 using namespace std;
-
-// A linked list node
-class Node {
+class Node{
 public:
     int data;
     Node* next;
 
-    // Constructor to initialize a new node with data
-    Node(int new_data) {
-        this->data = new_data;
-        this->next = nullptr;
+    Node(int new_data){
+        this->data=new_data;
+        this->next=nullptr;
     }
 };
-
-// Function to traverse and print the singly linked list
-void traverseList(Node* head) {
-
-    // A loop that runs till head is nullptr
-    while (head != nullptr) {
-
-        // Printing data of current node
-        cout << head->data << " ";
-
-        // Moving to the next node
-        head = head->next;
+void tranverselist(Node* head){
+    while( head !=nullptr){
+        cout << head->data<<" ";
+       head = head->next;
     }
-    cout << endl;
+     cout<<endl;
 }
+int main (){
+    Node *head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    
 
-// Driver Code
-int main() {
-  
-    // Create a hard-coded linked list:
-    // 10 -> 20 -> 30 -> 40
-    Node* head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
-    head->next->next->next = new Node(40);
-    head->next->next->next->next = new Node(50);
-    head->next->next->next->next->next = new Node(60);
-
-    // Example of traversing the node and printing
-    traverseList(head);
-
+    tranverselist(head);
     return 0;
 }
+
+
+
+
---
+++ linkedlist/tranversewithrecursion.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+// A linked list node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor to initialize a new node with data
+    Node(int new_data) {
+        this->data = new_data;
+        this->next = nullptr;
+    }
+};
+
+// Function to traverse and print the singly linked list
+void traverseList(Node* head) {
+
+    // Base condition is when the head is nullptr
+    if (head == nullptr) {
+        cout << endl;
+        return;
+    }
+
+    // Printing the current node data
+    cout << head->data << " ";
+
+    // Moving to the next node
+    traverseList(head->next);
+}
+
+// Driver code
+int main() {
+  
+    // Create a hard-coded linked list:
+    // 10 -> 20 -> 30 -> 40
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    // Example of traversing the node and printing
+    traverseList(head);
+
+    return 0;
+}
